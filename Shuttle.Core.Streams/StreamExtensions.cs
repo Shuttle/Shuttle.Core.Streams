b56e2f7baf61bd98a23ff94e96367e820521dc40
Zavarkó Gábor <zavarkog@gmail.com>
2018-10-11 16:43:15 +0200

    helper method which needs to avoid buffer allocations in ESB

--- Shuttle.Core.Streams/StreamExtensions.cs
+++ Shuttle.Core.Streams/StreamExtensions.cs
@@ -18,6 +18,25 @@ namespace Shuttle.Core.Streams
             }
         }
 
+        /// <summary>
+        ///     Copies the stream to a new MemoryStream object without copying the internal buffer.
+        ///     This method returning the same result as the <see cref="Copy"/> method when the instance it not a <see cref="MemoryStream"/>
+        /// </summary>
+        /// <param name="stream">Input stream</param>
+        /// <returns>Memory stream</returns>
+        public static MemoryStream CopyMemoryStream(this Stream stream)
+        {
+            Guard.AgainstNull(stream, nameof(stream));
+
+            if (stream is MemoryStream)
+            {
+                var ms = (MemoryStream) stream;
+                return new MemoryStream(ms.GetBuffer(), 0, (int) ms.Position);
+            }
+        
+            return (MemoryStream)Copy(stream);
+        }
+
         public static Stream Copy(this Stream stream)
         {
             Guard.AgainstNull(stream, nameof(stream));
